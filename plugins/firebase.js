89d21fb2cef2f7ad1a63c97c6cc217c3e9a31d72
h.kitajima <h.kitajima@lll-inc.co.jp>
2020-07-30 21:34:47 +0900

    firestore導入,認証,ログイン関係記載#3

---
+++ plugins/firebase.js
@@ -0,0 +1,27 @@
+// firebaseの機能使います宣言
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// firebaseのapps未設定である場合下記実行、情報は.envから持ってくる
+if(!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  }
+
+  firebase.initializeApp(firebaseConfig)
+}
+
+const db = firebase.firestore()
+
+export {
+  firebase,
+  db
+}

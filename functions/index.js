6067a18c0e7a94a0a1ce3c2dc7acc24c1fffaf7a
h.kitajima <h.kitajima@lll-inc.co.jp>
2020-11-11 00:20:27 +0900

    メンション通知機能でClass使用するよう修正 ・functions配下にmails.jsを作成し、Classを定義 ・index.jsにてmails.jsのClassを呼び出し、インスタンス生成

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin')
 const firebaseTools = require('firebase-tools')
 
 const nodemailer = require('nodemailer')
+const mentionNotice = require('./mails.js')
 const gmailEmail = functions.config().gmail.email
 const gmailPassword = functions.config().gmail.password
 const mailTransport = nodemailer.createTransport({
@@ -53,63 +54,9 @@ exports.deleteDocument = functions
 exports.sendMail = functions
   .https
   .onCall((data, context) => {
-    const email = {
-      from: gmailEmail,
-      to: data.email,
-      subject: 'チャットルームからのお知らせ',
-      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html lang="ja">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>チャットルームからのお知らせ</title>
-          <style type="text/css">
-            .siteName {
-              font-size: 24px;
-              text-align: center;
-            }
-            .main {
-              width: 40%;
-              padding: 0 30px 30px;
-              margin: 0 auto;
-              border: 1px solid #888;
-              border-radius: 10px;
-              text-align: center;
-            }
-            .message {
-              margin-bottom: 20px;
-              color: #444;
-              font-size: 18px;
-            }
-            .link {
-              display: block;
-              width: 30%;
-              padding: 10px 20px;
-              margin: 0 auto;
-              background: #601F69;
-              color: white;
-              font-weight: bold;
-              text-decoration: none;
-              border-radius: 5px;
-            }
-            a:visited,
-            a:active,
-            a:hover {
-              color: white;
-            }
-          </style>
-        </head>
-        <body>
-          <h1 class="siteName">チャットルーム</h1>
-          <div class="main">
-            <h2 class="message">${data.name}さんからメッセージが届いています。</h2>
-            <a class="link" href="https://chat-app-f1f7c.web.app/channels/${data.link}">メッセージを確認</a>
-          </div>
-        </body>
-        </html>`,
-    }
-    mailTransport.sendMail(email, (err, info) => {
+    const mail = new mentionNotice.SendMentionNotice(data)
+    const mailOptions =  mail.sendDescription
+    mailTransport.sendMail(mailOptions, (err, info) => {
       if (err) {
         return console.log(err)
       }
